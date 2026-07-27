ef32b96ef3e992ad1cac40cc7e1a19fa9532d74e
Vashu <vashuantaldhaliyan@gmail.com>
2026-07-27 15:08:32 +0530

    Added operators program

---
+++ Basics/basic_06.py
@@ -0,0 +1,30 @@
+a = 10
+b = 3
+
+# Arithmetic Operators
+print("----- Arithmetic Operators -----")
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Floor Division:", a // b)
+print("Modulus:", a % b)
+print("Exponent:", a ** b)
+
+#Comparison operators
+print("\n----- Comparison Operators -----")
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+#Logical operators
+print("\n----- Logical Operators -----")
+x = True
+y = False
+
+print(x and y)
+print(x or y)
+print(not x)
