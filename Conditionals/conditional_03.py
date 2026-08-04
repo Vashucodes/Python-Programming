852ecf0bffbcac7cd12cd2577fc42e829fad1f63
Vashu <vashuantaldhaliyan@gmail.com>
2026-08-04 20:50:32 +0530

    Added largest of two numbers program

---
+++ Conditionals/conditional_03.py
@@ -0,0 +1,9 @@
+num = int(input("Enter the first number:"))
+num1 = int(input("Enter the second number:"))
+
+if num > num1:
+    print(f"{num} is the greatest")
+elif num < num1:
+    print(f"{num1} is the greatest")
+else:
+    print("Both number are equal")
