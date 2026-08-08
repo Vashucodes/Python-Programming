d81f2d1bb0f9730edc08f041ecb0a778ae95a947
Vashu <vashuantaldhaliyan@gmail.com>
2026-08-08 21:19:00 +0530

    Added traingle validity program

---
+++ Conditionals/conditional_12.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the first angle:"))
+b = int(input("Enter the second angle:"))
+c = int(input("Enter the third angle:"))
+
+
+if a > 0 and b > 0 and c > 0 and a + b + c == 180:
+    print("Triangle is vaild")
+else:
+    print("not valid")
