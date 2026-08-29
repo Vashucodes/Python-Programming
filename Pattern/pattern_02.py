69680d99dec86cb2ba71576a4ac6b55a60953031
Vashu <vashuantaldhaliyan@gmail.com>
2026-08-29 20:30:03 +0530

    Added custom star pattern

---
+++ Pattern/pattern_02.py
@@ -0,0 +1,8 @@
+n = 5
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if i == 1 or j==1 or i==3 or i==n:
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
