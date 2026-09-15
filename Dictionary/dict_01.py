cb9efb559dbeb66eb96615e80af03fc3253b8a5d
Vashu <vashuantaldhaliyan@gmail.com>
2026-09-15 20:13:22 +0530

    Added progam to create and access dictionary

---
+++ Dictionary/dict_01.py
@@ -0,0 +1,10 @@
+student = {
+    "Name":"Aizen",
+    "Age":20,
+    "City":"Soul Society"
+}
+
+print("Dictionary:",student)
+print("Name:",student["Name"])
+print("Age:",student["Age"])
+print("City:",student["City"])
