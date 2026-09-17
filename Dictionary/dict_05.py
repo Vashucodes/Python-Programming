d9cd419e43b3fb5419ba6f84d319be3c13ef1a58
Vashu <vashuantaldhaliyan@gmail.com>
2026-09-17 22:20:40 +0530

    Added program to search dictionary key

---
+++ Dictionary/dict_05.py
@@ -0,0 +1,13 @@
+student = {
+    "Name":"Aizen",
+    "Age":20,
+    "City":"Soul Society"
+}
+
+key = input("Enter the key :")
+
+if key in student:
+    print("Key found")
+
+else:
+    print("Not found")
