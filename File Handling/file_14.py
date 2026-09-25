6befba914d131ff72118eb4701474a6d4a3b2b36
Vashu <vashuantaldhaliyan@gmail.com>
2026-09-25 22:50:12 +0530

    Added program to compare two file

---
+++ File Handling/file_14.py
@@ -0,0 +1,12 @@
+with open("file.txt") as f:
+    content1 = f.read()
+
+
+with open("text.txt") as f:
+    content2 = f.read()
+
+if(content1 == content2):
+    print("yes, these files are identical")
+
+else:
+    print("No, these files are not identical")
